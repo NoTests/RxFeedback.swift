de575d48e852cc44401c371d5a21619a3daf3eac
Orakaro <nhatminh_179@hotmail.com>
2017-11-12 10:36:53 +0900

    Get rid of identity State and compare event

--- Sources/RxFeedback/Reducer.swift
+++ Sources/RxFeedback/Reducer.swift
@@ -8,37 +8,33 @@
 
 import Foundation
 
-public class Reducer<State: Equatable, Event> {
+public class Reducer<State: Equatable, Event: Equatable> {
 
     typealias Transition = (Event) -> StateMonad<State>
     private var graph: [Transition] = []
-    private let identity: State
     private let identityMonad: StateMonad<State> = StateMonad { s in return s }
-    
-    public init(defaultState: State) {
-        identity = defaultState
-    }
+
+    public init() {}
 
     public func addEdge(event: Event, transform: @escaping (State) -> State) {
-        let transition: Transition = { event in
+        let transition: Transition = { [unowned self] e in
+            guard e == event else { return self.identityMonad }
             return StateMonad(f: transform)
         }
         graph.append(transition)
     }
 
-    public var reduce: (State, Event) -> State {
-        return { [unowned self] state, event in
-            let monad: StateMonad<State> = self.graph
-                .map { $0(event) }
-                .reduce(self.identityMonad) { lhs, rhs in
-                    return StateMonad { [unowned self] s in
-                        if lhs.run(s: s) != self.identity { return lhs.run(s: s) }
-                        if rhs.run(s: s) != self.identity { return rhs.run(s: s) }
-                        return self.identity
-                    }
+    public lazy var reduce: (State, Event) -> State = { [unowned self] state, event in
+        let monad: StateMonad<State> = self.graph
+            .map { $0(event) }
+            .reduce(self.identityMonad) { lhs, rhs in
+                return StateMonad { s in
+                    if lhs.run(s: s) != state { return lhs.run(s: s) }
+                    if rhs.run(s: s) != state { return rhs.run(s: s) }
+                    return state
                 }
-            return monad.run(s: state)
-        }
+            }
+        return monad.run(s: state)
     }
 
 }
